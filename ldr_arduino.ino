87d29acc7e83d1390fb180411aa016b379eceec8
lazaro camargo <lazaro.camargo@gmail.com>
2021-10-21 15:13:31 -0300

    Add files via upload

---
+++ ldr_arduino.ino
@@ -0,0 +1,18 @@
+// Palestra: Introducao aos sensores com o Tinkercad
+// Lazaro Camargo - INPE - SJC 
+// lazaro.camargo@inpe.br
+
+// programa para leitura do fotoresistor(LDR) com o Arduino
+
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int sensor = analogRead(A0); 
+  Serial.println(sensor); 
+  delay(10);
+}
